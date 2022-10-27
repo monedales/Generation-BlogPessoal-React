f9d288fc80feeef93723577d45a0c84bcf726a4c
Letícia Moneda <le.moneda@outlook.com>
2022-10-27 18:19:51 -0300

    Criação e estilização de componentes Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home()
+{
+    return (
+        <>
+            <h1 className='titulo' >Home</h1>
+            <img className='img' src='https://i.imgur.com/sJGBdWd.jpeg' alt='Mary mother of monsters' />
+        </>
+    );
+}
+
+export default Home;
