7dd44ada1d01a217c7489f672b0c70a6bba7cc4d
Letícia Moneda <le.moneda@outlook.com>
2022-11-08 12:24:27 -0300

    feat: task6-implementando de Temas e Postagens

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Typography, Grid, Button} from '@material-ui/core';
 import {Box} from '@mui/material';
 import './Home.css';
+import TabPostagem from '../../componentes/postagens/tabpostagem/TabPostagem';
 
 function Home()
 {
@@ -31,6 +32,7 @@ function Home()
                     <img src="https://i.imgur.com/sJGBdWd.jpeg" alt="" width="500px" height="500px" />
                 </Grid>
                 <Grid xs={12} className="postagens">
+                    <TabPostagem />
                 </Grid>
             </Grid>
         </>
